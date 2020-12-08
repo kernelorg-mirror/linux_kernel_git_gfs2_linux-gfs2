b0c042e19753740d8d8e03ac8e4b2a457e12c983
Bob Peterson <rpeterso@redhat.com>
2020-12-08 09:54:36 -0600

    gfs2: eliminate target parameter from do_xmote
    This is another baby step for state machine reform. Function do_xmote
    was passed in the target state, but in almost all cases gl->gl_target
    was used for the value.
    
    This patch eliminates the parameter so that do_xmote can be more atonomous.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -57,7 +57,7 @@ struct gfs2_glock_iter {
 
 typedef void (*glock_examiner) (struct gfs2_glock * gl);
 
-static void do_xmote(struct gfs2_glock *gl, unsigned int target);
+static void do_xmote(struct gfs2_glock *gl);
 
 static struct dentry *gfs2_root;
 static struct workqueue_struct *glock_workqueue;
@@ -529,7 +529,7 @@ static void finish_xmote(struct gfs2_glock *gl, unsigned int ret)
 					list_move_tail(&gh->gh_list, &gl->gl_holders);
 				gh = find_first_waiter(gl);
 				gl->gl_target = gh->gh_mode;
-				do_xmote(gl, gl->gl_target);
+				do_xmote(gl);
 				spin_unlock(&gl->gl_lockref.lock);
 				return;
 			}
@@ -544,12 +544,13 @@ static void finish_xmote(struct gfs2_glock *gl, unsigned int ret)
 		switch(mode) {
 		/* Unlocked due to conversion deadlock, try again */
 		case LM_ST_UNLOCKED:
-			do_xmote(gl, gl->gl_target);
+			do_xmote(gl);
 			break;
 		/* Conversion fails, unlock and try again */
 		case LM_ST_SHARED:
 		case LM_ST_DEFERRED:
-			do_xmote(gl, LM_ST_UNLOCKED);
+			gl->gl_target = LM_ST_UNLOCKED;
+			do_xmote(gl);
 			break;
 		default: /* Everything else */
 			fs_err(gl->gl_name.ln_sbd, "wanted %u got %u\n",
@@ -577,34 +578,26 @@ out_locked:
 /**
  * do_xmote - Calls the DLM to change the mode of a lock
  * @gl: The lock mode
- * @gh: The holder (only for promotes)
- * @target: The target lock mode
  *
  */
 
-static void do_xmote(struct gfs2_glock *gl, unsigned int target)
+static void do_xmote(struct gfs2_glock *gl)
 __releases(&gl->gl_lockref.lock)
 __acquires(&gl->gl_lockref.lock)
 {
 	const struct gfs2_glock_operations *glops = gl->gl_ops;
 	struct gfs2_sbd *sdp = gl->gl_name.ln_sbd;
-	struct gfs2_holder *gh = NULL;
-	unsigned int lck_flags = 0;
+	struct gfs2_holder *gh = find_first_waiter(gl);
+	unsigned int lck_flags = (unsigned int)(gh ? gh->gh_flags : 0);
+	int target = gl->gl_target;
 	int ret;
 
-	if (!test_bit(GLF_DEMOTE, &gl->gl_flags) ||
-	    gl->gl_demote_mode == gl->gl_mode) {
-		gh = find_first_waiter(gl);
-		if (gh)
-			lck_flags = (unsigned int)gh->gh_flags;
-	}
 	if (target != LM_ST_UNLOCKED && glock_blocked_by_withdraw(gl) &&
 	    gh && !(gh->gh_flags & LM_FLAG_NOEXP))
 		return;
 	lck_flags &= (LM_FLAG_TRY | LM_FLAG_TRY_1CB | LM_FLAG_NOEXP |
 		      LM_FLAG_PRIORITY);
 	GLOCK_BUG_ON(gl, gl->gl_mode == target);
-	GLOCK_BUG_ON(gl, gl->gl_mode == gl->gl_target);
 	if ((target == LM_ST_UNLOCKED || target == LM_ST_DEFERRED) &&
 	    glops->go_inval) {
 		/*
@@ -768,7 +761,7 @@ __acquires(&gl->gl_lockref.lock)
 		if (!(gh->gh_flags & (LM_FLAG_TRY | LM_FLAG_TRY_1CB)))
 			do_error(gl, 0); /* Fail queued try locks */
 	}
-	do_xmote(gl, gl->gl_target);
+	do_xmote(gl);
 }
 
 void gfs2_inode_remember_delete(struct gfs2_glock *gl, u64 generation)
