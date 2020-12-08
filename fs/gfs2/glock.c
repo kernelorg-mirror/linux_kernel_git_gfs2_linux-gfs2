311bb789457716d826b2baa4c8995ff51680a479
Bob Peterson <rpeterso@redhat.com>
2020-12-08 13:20:43 -0600

    gfs2: eliminate target parameter from finish_xmote
    Function finish_xmote took a parameter target, which was often set as
    from gl->gl_req. In glock_work_func it used the value of gl_reply, the
    reply from the dlm response to the request. This patch simplifies function
    finish_xmote by eliminating the target parameter in favor of just using
    gl->gl_req. This simplifies finish_xmote and prepares for it to ecome a
    state in a new state machine in future patches.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -500,14 +500,14 @@ static void gfs2_demote_wake(struct gfs2_glock *gl)
 /**
  * finish_xmote - The DLM has replied to one of our lock requests
  * @gl: The glock
- * @ret: The status from the DLM
  *
  */
 
-static void finish_xmote(struct gfs2_glock *gl, unsigned int ret)
+static void finish_xmote(struct gfs2_glock *gl)
 {
 	const struct gfs2_glock_operations *glops = gl->gl_ops;
 	struct gfs2_holder *gh;
+	int ret = gl->gl_req;
 	unsigned state = ret & LM_OUT_ST_MASK;
 	int rv;
 
@@ -694,14 +694,14 @@ skip_inval:
 		if (ret == -EINVAL && gl->gl_target == LM_ST_UNLOCKED &&
 		    target == LM_ST_UNLOCKED &&
 		    test_bit(SDF_SKIP_DLM_UNLOCK, &sdp->sd_flags)) {
-			finish_xmote(gl, target);
+			finish_xmote(gl);
 			gfs2_glock_queue_work(gl, 0);
 		} else if (ret) {
 			fs_err(sdp, "lm_lock ret %d\n", ret);
 			GLOCK_BUG_ON(gl, !gfs2_withdrawn(sdp));
 		}
 	} else { /* lock_nolock */
-		finish_xmote(gl, target);
+		finish_xmote(gl);
 		gfs2_glock_queue_work(gl, 0);
 	}
 out:
@@ -912,7 +912,8 @@ static void glock_work_func(struct work_struct *work)
 	unsigned int drop_refs = 1;
 
 	if (test_and_clear_bit(GLF_REPLY_PENDING, &gl->gl_flags)) {
-		finish_xmote(gl, gl->gl_reply);
+		gl->gl_req = gl->gl_reply;
+		finish_xmote(gl);
 		drop_refs++;
 	}
 	spin_lock(&gl->gl_lockref.lock);
