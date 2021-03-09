7ca999dada174d02d9f70b296c0126a2b587604b
Bob Peterson <rpeterso@redhat.com>
2021-03-09 09:47:13 -0500

    gfs2: Call state machine directly in clear_glock
    In this patch, we replace the queueing of glock work in function
    clear_glock with a direct call to the state machine.
    This will improve efficiency and reduce context switches.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -2132,8 +2132,8 @@ static void clear_glock(struct gfs2_glock *gl)
 	spin_lock(&gl->gl_lockref.lock);
 	if (gl->gl_state != LM_ST_UNLOCKED)
 		request_unlock(gl);
-	__gfs2_glock_queue_work(gl, 0);
-	spin_unlock(&gl->gl_lockref.lock);
+	if (!__state_machine(gl, GL_ST_RUN_QUEUE))
+		spin_unlock(&gl->gl_lockref.lock);
 }
 
 /**
