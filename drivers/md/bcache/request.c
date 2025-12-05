396e63a0de78505cfe130c7c769678be2b78deee
Andreas Gruenbacher <agruenba@redhat.com>
2025-12-05 19:08:20 +0000

    bio: use bio_io_error more often
    Instead of setting bio->bi_status to BLK_STS_IOERR and calling
    bio_endio(bio), use the shorthand bio_io_error(bio).
    
    Created with Coccinelle using the following semantic patch:
    
    @@
    struct bio *bio;
    @@
    - bio->bi_status = BLK_STS_IOERR;
    - bio_endio(bio);
    + bio_io_error(bio);
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- drivers/md/bcache/request.c
+++ drivers/md/bcache/request.c
@@ -1192,8 +1192,7 @@ void cached_dev_submit_bio(struct bio *bio)
 
 	if (unlikely((d->c && test_bit(CACHE_SET_IO_DISABLE, &d->c->flags)) ||
 		     dc->io_disable)) {
-		bio->bi_status = BLK_STS_IOERR;
-		bio_endio(bio);
+		bio_io_error(bio);
 		return;
 	}
 
@@ -1296,8 +1295,7 @@ void flash_dev_submit_bio(struct bio *bio)
 	struct bcache_device *d = bio->bi_bdev->bd_disk->private_data;
 
 	if (unlikely(d->c && test_bit(CACHE_SET_IO_DISABLE, &d->c->flags))) {
-		bio->bi_status = BLK_STS_IOERR;
-		bio_endio(bio);
+		bio_io_error(bio);
 		return;
 	}
 
--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -4090,8 +4090,7 @@ drop_bio:
 	if (fio->bio && *(fio->bio)) {
 		struct bio *bio = *(fio->bio);
 
-		bio->bi_status = BLK_STS_IOERR;
-		bio_endio(bio);
+		bio_io_error(bio);
 		*(fio->bio) = NULL;
 	}
 	return err;
